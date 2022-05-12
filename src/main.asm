c580adf410216e17a620a025a4181d48d9b330f8
Salsa Gal <fielddashos@gmail.com>
2022-05-12 22:21:01 +1000

    Add printing test data

--- src/main.asm
+++ src/main.asm
@@ -1,11 +1,25 @@
 section .text
 global _start
 _start:
+    mov byte [data], '%'
 
 interpret:
+    inc dword [instruction_pointer]
+
     mov eax, [instruction_pointer]      ; Get instruction address
     mov ebx, [data_pointer]             ; Get data address
     mov ch, [eax]                       ; Get instruction
+
+    cmp ch, '.'
+    jne .not_print
+
+.not_print:
+    mov eax, 4
+    mov ecx, [data_pointer]
+    mov ebx, 1
+    mov edx, 1
+    int 0x80
+    jnz interpret
 
 .end:
     mov eax, 1
@@ -14,8 +28,8 @@ interpret:
 
 section .data
     data_pointer: dd data
-    instruction_pointer: dd instructions
-    instructions: db "++++++++++++++++++++++++++++++++++++.", 0
+    instruction_pointer: dd instructions - 1
+    instructions: db "." 0
 
 section .bss
     data: resb 30000
