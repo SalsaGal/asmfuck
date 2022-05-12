bed3840c5798585577515be9c16481beeb15eefc
Salsa Gal <fielddashos@gmail.com>
2022-05-12 21:07:47 +1000

    Add printing data

--- src/main.asm
+++ src/main.asm
@@ -4,3 +4,18 @@ _start:
     mov eax, 1
     mov ebx, 0
     int 0x80
+
+print:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, data
+    add ecx, dword [data_pointer]
+    mov edx, 1
+    int 0x80
+    ret
+
+section .data
+    data_pointer: dd 0
+
+section .bss
+    data: resb 30000
