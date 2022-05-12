e46f315f4136f9fe899baaced4ff01697ac6f121
Salsa Gal <fielddashos@gmail.com>
2022-05-12 21:33:46 +1000

    Load data to registers

--- src/main.asm
+++ src/main.asm
@@ -4,31 +4,19 @@ _start:
 
 interpret:
     mov eax, [instruction_pointer]      ; Get instruction address
-    mov bl, [eax]                       ; Get instruction
-
-    cmp bl, 0
-    jz .end
-    inc dword [instruction_pointer]
-    jmp interpret
+    mov ebx, [data_pointer]
+    mov ch, [eax]
+    mov cl, [ebx]
 
 .end:
     mov eax, 1
     mov ebx, 0
     int 0x80
 
-print:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, data
-    add ecx, dword [data_pointer]
-    mov edx, 1
-    int 0x80
-    ret
-
 section .data
-    instructions: db "++++++++++++++++++++++++++++++++++++.", 0
+    data_pointer: dd data
     instruction_pointer: dd instructions
-    data_pointer: dd 0
+    instructions: db "++++++++++++++++++++++++++++++++++++.", 0
 
 section .bss
     data: resb 30000
